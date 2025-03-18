df46a51681a9d1e522f0ad8ec4b375c1d5db7303
Matt Williams <matthew.steven.williams@gmail.com>
2025-03-18 19:42:30 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests.py

---
+++ app.py
@@ -0,0 +1,2 @@
+def move_forward(x,y,direction,distance):
+    return {"x": 0, "y":20,"d":"N"}
--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
-def move_forward(x,y,direction,distance):
-    return {"x":0,"y":20,"d":"N"}
+from app import move_forward
+
+
 def test_translate_command():
-    assert move_forward(0,0,"N",20)=={"x":0,"y":20,"d":"N"}
+    assert move_forward(0, 0, "N", 20) == {"x": 0, "y": 20, "d": "N"}
+    assert move_forward(0, 0, "N", 10) == {"x": 0, "y": 10, "d": "N"}
