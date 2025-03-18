2029d5e63c09e0ecc53d53ce81a2023ff67d6e2f
fsohrabi <fs.sohrabi70@gmail.com>
2025-03-18 18:22:09 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:__pycache__/tests.cpython-312-pytest-8.3.5.pyc

--- tests.py
+++ tests.py
@@ -1,2 +1,4 @@
+def move_forward(x,y,direction,distance):
+    return {"x":0,"y":20,"d":"N"}
 def test_translate_command():
-    assert translate_command('x', 'y')
+    assert move_forward(0,0,"N",20)=={"x":0,"y":20,"d":"N"}
