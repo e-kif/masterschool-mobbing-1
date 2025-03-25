e29652a4a5d7f0d75b925a6cede3d99e5d2271ba
e-kif <e.kifiuk@gmail.com>
2025-03-25 11:30:00 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:app.py

--- app.py
+++ app.py
@@ -1,2 +1,3 @@
-def move_forward(x,y,direction,distance):
-    return {"x": 0, "y":20,"d":"N"}
+def move_forward(x, y, direction, distance):
+
+    return {"x": 0, "y": distance, "d": direction}
--- tests.py
+++ tests.py
@@ -4,3 +4,4 @@ from app import move_forward
 def test_translate_command():
     assert move_forward(0, 0, "N", 20) == {"x": 0, "y": 20, "d": "N"}
     assert move_forward(0, 0, "N", 10) == {"x": 0, "y": 10, "d": "N"}
+    assert move_forward(0, 10, "N", 10) == {"x": 0, "y": 20, "d": "N"}
