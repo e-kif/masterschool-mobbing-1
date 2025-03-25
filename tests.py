61957700b0d973876e61d455213f03959f239e35
e-kif <e.kifiuk@gmail.com>
2025-03-25 12:12:51 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
-from app import move_forward, turn_right
+from app import move_forward, turn_right, turn_left
 
 
-def test_translate_command():
+def test_move_forward():
     assert move_forward(0, 0, "N", 20) == {"x": 0, "y": 20, "d": "N"}
     assert move_forward(0, 0, "N", 10) == {"x": 0, "y": 10, "d": "N"}
     assert move_forward(0, 10, "N", 10) == {"x": 0, "y": 20, "d": "N"}
@@ -24,3 +24,12 @@ def test_turn_command():
     assert turn_right(0, 0, "E") == {"x": 0, "y": 0, "d": "S"}
     assert turn_right(0, 10, "S") == {"x": 0, "y": 10, "d": "W"}
     assert turn_right(0, 10, "W") == {"x": 0, "y": 10, "d": "N"}
+
+    assert turn_left(0, 0, "N") == {"x": 0, "y": 0, "d": "W"}
+    assert turn_left(0, 0, "E") == {"x": 0, "y": 0, "d": "N"}
+    assert turn_left(0, 10, "S") == {"x": 0, "y": 10, "d": "E"}
+    assert turn_left(0, 10, "W") == {"x": 0, "y": 10, "d": "S"}
+
+
+def test_move_backward():
+    pass
