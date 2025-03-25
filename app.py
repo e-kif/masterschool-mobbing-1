f1ac8767de76f6fe4a4f40bbbc764d94c4de4e7d
Shahriar Fakhar <chomb@Shahriars-MacBook-Pro.fritz.box>
2025-03-25 12:06:11 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:__pycache__/app.cpython-313.pyc

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def move_forward(x, y, direction, distance):
         "x": x,
         "y": y,
         "d": direction
-            }
+    }
     if direction == "N":
         data["y"] += distance
     elif direction == "S":
@@ -16,3 +16,22 @@ def move_forward(x, y, direction, distance):
         print("wrong direction....")
     return data
 
+
+def turn_right(x, y, direction):
+    mapping = {
+        "N": "E",
+        "E": "S",
+        "S": "W",
+        "W": "N"
+    }
+    return {"x": x, "y": y, "d": mapping[direction]}
+
+
+def turn_left(x, y, direction):
+    mapping = {
+        "N": "W",
+        "E": "N",
+        "S": "E",
+        "W": "S"
+    }
+    return {"x": x, "y": y, "d": mapping[direction]}
--- tests.py
+++ tests.py
@@ -1,7 +1,26 @@
-from app import move_forward
+from app import move_forward, turn_right
 
 
 def test_translate_command():
     assert move_forward(0, 0, "N", 20) == {"x": 0, "y": 20, "d": "N"}
     assert move_forward(0, 0, "N", 10) == {"x": 0, "y": 10, "d": "N"}
     assert move_forward(0, 10, "N", 10) == {"x": 0, "y": 20, "d": "N"}
+
+    assert move_forward(0, 0, "S", 20) == {"x": 0, "y": -20, "d": "S"}
+    assert move_forward(0, 0, "S", 10) == {"x": 0, "y": -10, "d": "S"}
+    assert move_forward(0, 10, "S", 10) == {"x": 0, "y": 0, "d": "S"}
+
+    assert move_forward(0, 0, "W", 20) == {"x": -20, "y": 0, "d": "W"}
+    assert move_forward(0, 0, "W", 10) == {"x": -10, "y": 0, "d": "W"}
+    assert move_forward(0, 10, "W", 10) == {"x": -10, "y": 10, "d": "W"}
+
+    assert move_forward(0, 0, "E", 20) == {"x": 20, "y": 0, "d": "E"}
+    assert move_forward(0, 0, "E", 10) == {"x": 10, "y": 0, "d": "E"}
+    assert move_forward(0, 10, "E", 10) == {"x": 10, "y": 10, "d": "E"}
+
+
+def test_turn_command():
+    assert turn_right(0, 0, "N") == {"x": 0, "y": 0, "d": "E"}
+    assert turn_right(0, 0, "E") == {"x": 0, "y": 0, "d": "S"}
+    assert turn_right(0, 10, "S") == {"x": 0, "y": 10, "d": "W"}
+    assert turn_right(0, 10, "W") == {"x": 0, "y": 10, "d": "N"}
