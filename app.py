19a432944333d25c2eafcd929ae7da0616b2964f
Matt Williams <matthew.steven.williams@gmail.com>
2025-03-25 11:42:56 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 def move_forward(x, y, direction, distance):
+    data = {
+        "x": x,
+        "y": y,
+        "d": direction
+            }
+    if direction == "N":
+        data["y"] += distance
+    elif direction == "S":
+        data["y"] -= distance
+    elif direction == "E":
+        data["x"] += distance
+    elif direction == "W":
+        data["x"] -= distance
+    else:
+        print("wrong direction....")
+    return data
 
-    return {"x": 0, "y": distance, "d": direction}
