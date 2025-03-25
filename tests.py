a2459147f7b93d81e95f28c3c4c6d0722acdc82f
Matt Williams <matthew.steven.williams@gmail.com>
2025-03-25 12:22:06 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:tests.py

--- tests.py
+++ tests.py
@@ -32,4 +32,18 @@ def test_turn_command():
 
 
 def test_move_backward():
-    pass
+    assert move_backwards(0, 0, "N", 20) == {"x": 0, "y": -20, "d": "N"}
+    assert move_backwards(0, 0, "N", 10) == {"x": 0, "y": -10, "d": "N"}
+    assert move_backwards(0, 10, "N", 10) == {"x": 0, "y": 0, "d": "N"}
+
+    assert move_backwards(0, 0, "S", 20) == {"x": 0, "y": 20, "d": "S"}
+    assert move_backwards(0, 0, "S", 10) == {"x": 0, "y": 10, "d": "S"}
+    assert move_backwards(0, 10, "S", 10) == {"x": 0, "y": 0, "d": "S"}
+
+    assert move_backwards(0, 0, "W", 20) == {"x": -20, "y": 20, "d": "W"}
+    assert move_backwards(0, 0, "W", 10) == {"x": -10, "y": 0, "d": "W"}
+    assert move_backwards(0, 10, "W", 10) == {"x": -10, "y": 10, "d": "W"}
+
+    assert move_backwards(0, 0, "E", 20) == {"x": 20, "y": 0, "d": "E"}
+    assert move_backwards(0, 0, "E", 10) == {"x": 10, "y": 0, "d": "E"}
+    assert move_backwards(0, 10, "E", 10) == {"x": 10, "y": 10, "d": "E"}
